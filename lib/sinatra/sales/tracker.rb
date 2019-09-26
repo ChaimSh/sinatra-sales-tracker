0c82f34adc83fdaa7a71023e30e4cfad24142eca
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-09-26 00:42:07 +0000

    first commit

---
+++ lib/sinatra/sales/tracker.rb
@@ -0,0 +1,10 @@
+require "sinatra/sales/tracker/version"
+
+module Sinatra
+  module Sales
+    module Tracker
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/sinatra/sales/tracker/version.rb
@@ -0,0 +1,7 @@
+module Sinatra
+  module Sales
+    module Tracker
+      VERSION = "0.1.0"
+    end
+  end
+end
